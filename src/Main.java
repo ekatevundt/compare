8b215419b6c4b662ea7b0c820be1c97f68f926d0
ekatevundt <ekatevundt@yandex.ru>
2022-10-10 11:23:32 +0300

    Lambda added

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,12 @@
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 
 public class Main {
+
+
     public static void main(String[] args) {
         List<Person> people = new ArrayList<>();
         people.add(new Person("Olaf", "von Sholz", 22));
@@ -12,9 +15,27 @@ public class Main {
         people.add(new Person("Anton", "Petrov", 45));
         people.add(new Person("Anyon", "Con com chu", 11));
 
-        System.out.println(people);
-        Collections.sort(people, new PeopleComparator(3));
-        System.out.println(people);
+        Comparator<Person> comparator = (Person x, Person y) -> {
+            String[] surnames = x.getSurname().split(" ");
+            String[] oSurnames = y.getSurname().split(" ");
+            int maxWords = 3;
+            if (maxWords <= surnames.length && maxWords <= oSurnames.length) {
+                return y.getAge() - x.getAge();
+            } else if (surnames.length > oSurnames.length) {
+                return -1;
+            } else if (surnames.length < oSurnames.length) {
+                return 1;
+            } else {
+                return y.getAge() - x.getAge();
+            }
+        };
+        sortList(people, comparator);
+    }
+    public static <T> void sortList(List<T> list, Comparator<T> comparator) {
+        list.sort(comparator);
+        list.forEach(System.out::println);
 
     }
+
+
 }
